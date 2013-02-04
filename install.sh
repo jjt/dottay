f17396b492db12c45bcdbc3bc571e51e8770c7f4
Jason Trill <jason@jasontrill.com>
2013-02-04 01:08:27 -0800

    Updated install.sh

--- install.sh
+++ install.sh
@@ -15,29 +15,10 @@ if [ "$1" = "vim" ]; then
     do
        link_file $i
     done
+    git clone git://github.com/gmarik/vundle.git _vim/bundle/vundle
 else
     for i in _*
     do
         link_file $i
     done
 fi
-
-git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
-    while read path_key path
-    do
-        url_key=$(echo $path_key | sed 's/\.path/.url/')
-        url=$(git config -f .gitmodules --get "$url_key")
-        git submodule add $url $path
-    done
-
-
-git submodule sync
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
-git submodule foreach git submodule init
-git submodule foreach git submodule update
-
-# setup command-t
-cd _vim/bundle/command-t
-rake make
